a0bd10297f90c423e69d6637a901649e08ad4f41
0xFF <luq5nvd@gmail.com>
2023-04-04 11:56:02 +0700

    create route - read and delete

--- Route/DivisionUser.js
+++ Route/DivisionUser.js
@@ -6,10 +6,27 @@ const divisionUserModel = require('../Model/DivisionUserModel')
 
 router.route('/')
     .post((req, res) => {
-        const divisionName = req.body.divisionName
+        const divisionName = _.upperCase(req.body.divisionName)
         divisionUserModel.insertDivisionUser(
             200,
             divisionName,
+            res
+        )
+    })
+
+    .get((req, res) => {
+        divisionUserModel.findAllDivisionUser(
+            200,
+            `successfully get all division user model`,
+            res
+        )
+    })
+
+    .delete((req, res) => {
+        const idDivision = req.body.idDivision
+        divisionUserModel.deleteDivisionUser(
+            200,
+            idDivision,
             res
         )
     })
