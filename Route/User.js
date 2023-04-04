120142d8360e2452d52657d2189e43749bfc66ee
0xFF <luq5nvd@gmail.com>
2023-04-04 14:09:43 +0700

    create insert method

---
+++ Route/User.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+const _ = require('lodash')
+
+const userModel = require('../Model/UserModel')
+
+router.route('/')
+    .post((req, res) => {
+        const userName = _.upperCase(req.body.userName)
+        const userPassword = req.body.userPassword
+        const userEmail = req.body.userEmail
+        const userPhoneNumber = req.body.userPhoneNumber
+        const roleId = req.body.roleId
+        const activeId = req.body.activeId
+        const divisionId = req.body.divisionId
+
+        userModel.insertUser(
+            200,
+            userName,
+            userPassword,
+            userEmail,
+            userPhoneNumber,
+            roleId,
+            activeId,
+            divisionId,
+            res
+        )
+    })
+
+module.exports = router
