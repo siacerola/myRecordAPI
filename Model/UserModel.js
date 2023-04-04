122d7c963a34ad05dd8c6d8561b3e5f48c53a5a0
0xFF <luq5nvd@gmail.com>
2023-04-04 13:53:16 +0700

    create method userModel

--- Model/UserModel.js
+++ Model/UserModel.js
@@ -1,4 +1,5 @@
-
+const mongoose = require('mongoose')
+const { Schema } = mongoose
 
 
 const userSchema = new Schema({
@@ -18,6 +19,63 @@ const userSchema = new Schema({
     userPhoneNumber: {
         type: String,
         required:true
+    },
+    fkRoleId: {
+        type: Schema.Types.ObjectId,
+        ref:"Role"
+    },
+    fkActiveId: {
+        type: Schema.Types.ObjectId,
+        ref:"Active"
+    },
+    fkDivisionId: {
+        type: Schema.Types.ObjectId,
+        ref:"Division"
+    },
+    cretatedDate: {
+        type:Date
     }
-
 })
+
+const User = mongoose.model("User", userSchema)
+
+const insertOne = async (
+    statusCode,
+    userName,
+    userPassword,
+    userEmail,
+    userPhoneNumber,
+    roleId,
+    activeId,
+    divisionId,
+    createdDate,
+    res
+) => {
+    const newUser = new User()
+
+    newUser.userName = userName
+    newUser.userPassword = userPassword
+    newUser.userEmail = userEmail
+    newUser.userPhoneNumber = userPhoneNumber
+    newUser.fkRoleId = roleId
+    newUser.fkActiveId = activeId
+    newUser.fkDivisionId = divisionId
+    newUser.cretatedDate = createdDate
+    
+    const saveUser = await newUser.save()
+
+    let saveStatus=""
+    if (newUser === saveUser) {
+        saveStatus="successfully to save"
+    } else {
+        saveStatus="failed to save"
+    }
+    res.status(statusCode).json({
+        message: `${newUser.userName} ${saveStatus}`,
+        statusCode:statusCode
+    })
+}
+
+module.exports = {
+    insertUser:insertOne
+}
